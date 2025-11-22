40ea2d2005b8a41bac6c4611d9c60b1ae9f83c83
plabondas001 <plabondas001@gmail.com>
2025-11-22 13:22:29 +0600

    feet to inch

--- inch_to_feet.js
+++ inch_to_feet.js
@@ -12,4 +12,13 @@ console.log(parseInt(feet));
 
 
 
+// feet to inch
 
+
+function feetToInch(feet){
+    let inch = feet * 12;
+    return inch;
+}
+
+let inch = feetToInch(5);
+console.log(inch);
